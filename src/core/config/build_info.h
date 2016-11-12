feef08aef5e9d14ec1e3f0ebad333d6b9f18b99c
ropelinen <ropelinen@gmail.com>
2016-11-12 21:15:43 +0200

    Core: Added generic defines based on the current build type and platform.

---
+++ src/core/config/build_info.h
@@ -0,0 +1,28 @@
+#ifndef DEA_BUILD_INFO_H
+#define DEA_BUILD_INFO_H
+
+/* A bunch of defines based on the current build configuration and platform */
+
+/* Platform defines 0x2 - 0x20 */
+#if defined (_WIN32) || defined(_WIN64)
+#define DEA_PLATFORM DEA_PLATFORM_WINDOWS
+#else
+#error "Platform not defined" 
+#endif
+
+/* Build configuration 0x100 - 0x110 */
+#define DEA_DEBUG 0x100
+#define DEA_RELEASE 0x105
+#define DEA_PRODUCTION 0x110
+
+#if defined (CONF_DEBUG)
+#define DEA_BUILD_TYPE RPLNN_DEBUG
+#elif defined (CONF_RELEASE)
+#define DEA_BUILD_TYPE DEA_RELEASE
+#elif defined (CONF_PRODUCTION)
+#define DEA_BUILD_TYPE DEA_PRODUCTION
+#else
+#error "Build configuration not defined"
+#endif
+
+#endif // DEA_BUILD_INFO_H
--- src/core/config/precompiled.h
+++ src/core/config/precompiled.h
@@ -1,6 +1,7 @@
 #ifndef DEA_PRECOMPILED_H
 #define DEA_PRECOMPILED_H
 
+#include "core/config/build_info.h"
 #include "core/config/disables_msvc.h"
 
 #endif // DEA_PRECOMPILED_H
