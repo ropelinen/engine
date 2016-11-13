756f8e5f3815c096b5c30d7e1a287592a970297f
ropelinen <ropelinen@gmail.com>
2016-11-13 15:37:10 +0200

    Engine: Added empty system and system_windows files to engine project.

---
+++ src/engine/system/system.h
@@ -0,0 +1,10 @@
+#ifndef DEA_ENGINE_SYSTEM_H
+#define DEA_ENGINE_SYSTEM_H
+
+DEA_START()
+
+void create_window();
+
+DEA_END()
+
+#endif // DEA_ENGINE_SYSTEM_H
---
+++ src/engine/system/system_windows.cpp
@@ -0,0 +1,14 @@
+#include "engine/config/precompiled.h"
+
+#if (DEA_PLATFORM == DEA_PLATFORM_WINDOWS)
+#include "engine/system/system.h"
+
+DEA_START()
+
+void create_window()
+{
+}
+
+DEA_END()
+
+#endif // DEA_PLATFORM == DEA_PLATFORM_WINDOWS
