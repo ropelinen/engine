62674cd74b44a701a3c381ed16297ae2fb626c63
ropelinen <ropelinen@gmail.com>
2016-11-13 15:29:43 +0200

    Core: Added defines for my namespace to lessen the chance of typos.

--- src/core/config/config.h
+++ src/core/config/config.h
@@ -2,9 +2,11 @@
 #define DEA_CORE_CONFIG_H
 
 /* Generic defines */
-
 #if defined(_MSC_VER)
 #define WIN32_LEAN_AND_MEAN
 #endif
+
+#define DEA_START() namespace dea {
+#define DEA_END() } // namespace dea
 
 #endif // DEA_CORE_CONFIG_H
