e97c6bc162788ec14fdd47fcce425c097029e1fc
ropelinen <ropelinen@gmail.com>
2016-11-13 15:30:30 +0200

    Core: Added int type defines uint8_t -> uint8 etc

---
+++ src/core/types/int_types.h
@@ -0,0 +1,16 @@
+#ifndef DEA_CORE_INT_TYPES_H
+#define DEA_CORE_INT_TYPES_H
+
+#include <stdint.h>
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef unsigned int uint;
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+#endif // DEA_CORE_INT_TYPES_H
