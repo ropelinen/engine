4040d5803dd357c4ec01db6040a3fb748e89afde
ropelinen <ropelinen@gmail.com>
2016-12-03 23:41:42 +0200

    Demo: Create and destroy two windows

--- src/demo/system/main.cpp
+++ src/demo/system/main.cpp
@@ -1,16 +1,34 @@
 #include "demo/system/precompiled.h"
+#include "engine/system/system.h"
+
+DEA_START()
 
 #if (DEA_PLATFORM == DEA_PLATFORM_WINDOWS)
 #include <windows.h>
-#endif
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prev_instance, PSTR cmd_line, int cmd_show)
 {
 	/* Just the get this to compile for now */
-	(void*)hInstance;
-	(void*)hPrevInstance;
-	(void*)pScmdline;
-	--iCmdshow;
+	(void*)hinstance;
+	(void*)prev_instance;
+	(void*)cmd_line;
+	--cmd_show;
+
+	const bool fullscreen = false;
+	window windows[2];
+	create_window(1280, 720, 0.5f, 0.5f, L"Engine", fullscreen, windows[0]);
+	create_window(600, 400, 0.0f, 0.0f, L"Editor", fullscreen, windows[1]);
+	focus_window(windows[0]);
+
+	run();
+
+	destroy_window(windows[0], fullscreen);
+	destroy_window(windows[1], fullscreen);
 
 	return 0;
 }
+#else
+#error No Main for current platform
+#endif
+
+DEA_END()
