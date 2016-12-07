e5329ebf80cc6f12edc42622228999bf9fceb264
ropelinen <ropelinen@gmail.com>
2016-12-07 18:20:29 +0200

    Core: Added intptr and uintptr typedefines.

--- src/core/types/int_types.h
+++ src/core/types/int_types.h
@@ -12,5 +12,7 @@ typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
+typedef uintptr_t uintptr;
+typedef intptr_t intptr;
 
 #endif // DEA_CORE_INT_TYPES_H
