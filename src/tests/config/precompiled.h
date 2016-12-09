fc4f1533ce6372362398200850507270fe5a8fac
ropelinen <ropelinen@gmail.com>
2016-12-09 22:38:03 +0200

    Tests: Add project for unit tests

---
+++ src/tests/config/precompiled.h
@@ -0,0 +1,6 @@
+#ifndef DEA_TESTS_PRECOMPILED_H
+#define DEA_TESTS_PRECOMPILED_H
+
+#include "core/config/precompiled.h"
+
+#endif // DEA_TESTS_PRECOMPILED_H
---
+++ src/tests/main.cpp
@@ -0,0 +1,10 @@
+#include "tests/config/precompiled.h"
+
+int main(char **argv, int argc)
+{
+	/* Just to get this to compile */
+	(void *)argv;
+	--argc;
+
+	return 0;
+}
