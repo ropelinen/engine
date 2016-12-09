8cd613fd48e67dc14b3f6138b5c88448ac312958
ropelinen <ropelinen@gmail.com>
2016-12-09 22:37:17 +0200

    Core: Add is (not) pod asserts.

--- src/core/system/assert.h
+++ src/core/system/assert.h
@@ -6,13 +6,18 @@
 #if (DEA_ASSERT_ENABLED == DEA_FALSE)
 #define dea_assert(condition) ((void)0)
 #define dea_static_assert(condition) ((void)0)
+#define dea_is_pod_assert(type) ((void)0)
+#define dea_is_not_pod_assert(type) ((void)0)
 #elif (DEA_ASSERT_ENABLED == DEA_TRUE)
 	#if (DEA_PLATFORM == DEA_PLATFORM_WINDOWS)
 	/* Just use the default assert for now,
 	 * Easier to add funtionality later if I'm already using my own assert. */
 	#include <assert.h>
+	#include <type_traits>
 	#define dea_assert(condition) assert(condition)
 	#define dea_static_assert(condition) static_assert(condition)
+	#define dea_is_pod_assert(type) dea_static_assert(std::is_trivially_copyable<type>::value)
+	#define dea_is_not_pod_assert(type) dea_static_assert(!std::is_trivially_copyable<type>::value)
 	#else
 	#error dea_assert not defined for current platform
 	#endif
