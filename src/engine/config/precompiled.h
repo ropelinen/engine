165e46e5d53f50ed8ead78e941de0db3ba2045f6
ropelinen <ropelinen@gmail.com>
2016-12-23 17:39:41 +0200

    Engine: Added vector and pod_vector to precompiled headers

--- src/engine/config/precompiled.h
+++ src/engine/config/precompiled.h
@@ -2,7 +2,9 @@
 #define DEA_ENGINE_PRECOMPILED_H
 
 #include "core/config/precompiled.h"
-
 #include "engine/config/config.h"
+
+#include "core/container/pod_vector.h"
+#include "core/container/vector.h"
 
 #endif // DEA_ENGINE_PRECOMPILED_H
