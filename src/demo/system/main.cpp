ebb3b53a3dd97b789308fea04f4f52b46797c80a
ropelinen <ropelinen@gmail.com>
2017-02-24 18:00:07 +0200

    Demo: Demo now uses pod_vector to store windows.

--- src/demo/system/main.cpp
+++ src/demo/system/main.cpp
@@ -14,16 +14,24 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prev_instance, PSTR cmd_line, 
 	(void*)cmd_line;
 	--cmd_show;
 
+	init_system_data();
+
 	const bool fullscreen = false;
-	window windows[2];
-	create_window(1280, 720, 0.5f, 0.5f, L"Engine", fullscreen, windows[0]);
-	create_window(600, 400, 0.0f, 0.0f, L"Editor", fullscreen, windows[1]);
+	pod_vector<window> windows;
+	{
+		window temp;
+		windows.resize(2, temp);
+		create_window(1280, 720, 0.5f, 0.5f, L"Engine", fullscreen, windows[0]);
+		create_window(600, 400, 0.0f, 0.0f, L"Editor", fullscreen, windows[1]);
+	}
 	focus_window(windows[0]);
 
-	run();
+	run(windows);
 
-	destroy_window(windows[0], fullscreen);
-	destroy_window(windows[1], fullscreen);
+	for (auto it = windows.get_begin(); it != windows.get_end(); ++it)
+		destroy_window(*it, fullscreen);
+
+	destroy_system_data();
 
 	return 0;
 }
